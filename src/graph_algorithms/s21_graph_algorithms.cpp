3a0607ab34181eb0a90706adcee7a55465b66563
Sabina <evetteis@student.21-school.ru>
2023-11-26 01:24:30 +0300

    gabriela GraphAlgorithms init commit

---
+++ src/graph_algorithms/s21_graph_algorithms.cpp
@@ -0,0 +1,48 @@
+#include "s21_graph_algorithms.h"
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+int* GraphAlgorithms::DepthFirstSearch(Graph &graph, int start_vertex) {
+    std::vector<int> traversed_vertices;
+    std::stack<int> s;
+    s.push(start_vertex);
+    while (!s.empty()) {
+        int src = s.top();
+        if (std::count(traversed_vertices.begin(), traversed_vertices.end(), src)) {
+            s.pop();
+            continue;
+        }
+        traversed_vertices.push_back(src);
+        s.pop();
+        for (int dest : graph.Destinations(src)) {
+            s.push(dest);
+        }
+    }
+    // for (int i : traversed_vertices) {
+    //     std::cout << i;
+    // }
+    // std::cout << std::endl;
+    return traversed_vertices.data();
+}
+
+int* GraphAlgorithms::BreadthFirstSearch(Graph &graph, int start_vertex) {
+    std::vector<int> traversed_vertices;
+    std::queue<int> q;
+    q.push(start_vertex);
+    while (!q.empty()) {
+        int src = q.front();
+        if (std::find(traversed_vertices.begin(), traversed_vertices.end(), src) == traversed_vertices.end()) {
+            traversed_vertices.push_back(src);
+            for (int dest : graph.Destinations(src)) {
+                q.push(dest);
+            }
+        }
+        q.pop();
+    }
+    // for (int i : traversed_vertices) {
+    //     std::cout << i;
+    // }
+    // std::cout << std::endl;
+    return &traversed_vertices[0];
+}
